593c022d39f637421171ee28ee47b83df5955d94
Paul Duncan <pabs@pablotron.org>
2026-02-15 01:51:20 -0500

    add examples/decode.rs

---
+++ examples/decode.rs
@@ -0,0 +1,33 @@
+//! Decode [Bech32][] string from first command-line output, convert the
+//! decoded data to a string, then print the string to standard output.
+//!
+//! # Example
+//!
+//! ```sh
+//! $ cargo run -q --example decode -- foo1vfshy2dnlu3
+//! bar
+//! ```
+//!
+//! [bech32]: https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
+//!   "Bech32 (BIP173)"
+
+use pbech32::{Bech32, Err};
+
+/// Strip trailing 0 bytes and convert vector of bytes to string.
+fn stringify(v: Vec<u8>) -> String {
+  v.iter().filter(|b| **b != 0).map(|b| *b as char).collect::<String>()
+}
+
+fn main() -> Result<(), Err> {
+  // get/check args
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() != 2 {
+    panic!("Usage: {} [BECH32_STR]", args[0]);
+  }
+
+  // decode arg, convert data to string, then print string
+  let b: Bech32 = args[1].parse()?; // decode arg
+  println!("{}", stringify(b.data)); // convert/print string
+
+  Ok(())
+}
