c75e2fdef4bb1a9b8fd4bd49edc47e2224023cb0
Paul Duncan <pabs@pablotron.org>
2026-03-01 00:05:07 -0500

    examples/hello-world.rs: simplify

--- examples/hello-world.rs
+++ examples/hello-world.rs
@@ -4,26 +4,24 @@
 //! # Example
 //! 
 //! ```sh
-//! $ cargo run --example hello-world
+//! $ cargo run -q --example hello-world
 //! encoded = hello1vehkc6mn27xpct
 //! decoded = Bech32 { scheme: Bech32m, hrp: Hrp("hello"), data: [102, 111, 108, 107, 115] }
 //! ```
 //!
 //! [bech32]: https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
 //!   "Bech32 (BIP173)"
-use pbech32::{Bech32, Err, Scheme};
+use pbech32::{Bech32, Err, Hrp, Scheme};
 
 fn main() -> Result<(), Err> {
-  // encode as bech32m string
-  let s = Bech32 {
-    scheme: Scheme::Bech32m, // checksum scheme
-    hrp: "hello".parse()?, // human-readable part
-    data: b"folks".to_vec(), // data
-  }.to_string();
+  let scheme = Scheme::Bech32m; // checksum scheme
+  let hrp: Hrp = "hello".parse()?; // human-readable part
+  let data = b"folks".to_vec(); // data
+  let s = Bech32 { scheme, hrp, data }.to_string(); // encode string
   
   println!("encoded = {s}"); // print string
   let b: Bech32 = s.parse()?; // parse string
   println!("decoded = {b:?}"); // print struct
 
-  Ok(())
+  Ok(()) // return success
 }
